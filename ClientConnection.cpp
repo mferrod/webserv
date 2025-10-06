7ed97f0fa0ae87b80e336e467ad006cf6f3bdb1e
Germán Fredes <germanfredes1988@protonmail.com>
2025-10-07 00:00:46 +0200

    Add structs(HttpRequest, HttpResponse) and parseRequest()

--- ClientConnection.cpp
+++ ClientConnection.cpp
@@ -17,9 +17,15 @@ bool ClientConnection::readData() {
 	if (bytesRead <= 0) return false;
 
 	_buffer.append(buf, bytesRead);
-
+	// For debugging
+	/* _buffer = "GET /hola.html HTTP/1.1\r\n"
+	"Host: localhost\r\n"
+	"User-Agent: test-client\r\n"
+	"\r\n"
+	"<html><body>Hello, world!</body></html>"; */
 	if (_buffer.find("\r\n\r\n") != std::string::npos) {
 		_complete = true;
+		_request = parseRequest(_buffer);
 	}
 	return true;
 }
@@ -34,4 +40,48 @@ bool ClientConnection::sendResponse(const std::string &response) {
 		totalSent += sent;
 	}
 	return true;
+}
+
+HttpRequest ClientConnection::parseRequest(const std::string &rawRequest) {
+	HttpRequest request;
+	size_t pos = 0;
+	size_t lineEnd = rawRequest.find("\r\n");
+
+	if (lineEnd == std::string::npos) return request;
+
+	std::string requestLine = rawRequest.substr(0, lineEnd);
+	pos = lineEnd + 2;
+
+	size_t methodEnd = requestLine.find(' ');
+	if (methodEnd == std::string::npos) return request;
+	request.method = requestLine.substr(0, methodEnd);
+
+	size_t pathEnd = requestLine.find(' ', methodEnd + 1);
+	if (pathEnd == std::string::npos) return request;
+	request.path = requestLine.substr(methodEnd + 1, pathEnd - methodEnd - 1);
+
+	request.version = requestLine.substr(pathEnd + 1);
+
+	while (true) {
+		lineEnd = rawRequest.find("\r\n", pos);
+		if (lineEnd == std::string::npos || lineEnd == pos) break;
+
+		std::string headerLine = rawRequest.substr(pos, lineEnd - pos);
+		size_t colonPos = headerLine.find(':');
+		if (colonPos != std::string::npos) {
+			std::string headerName = headerLine.substr(0, colonPos);
+			std::string headerValue = headerLine.substr(colonPos + 1);
+			while (!headerValue.empty() && (headerValue[0] == ' ' || headerValue[0] == '\t')) {
+				headerValue.erase(0, 1);
+			}
+			request.headers[headerName] = headerValue;
+		}
+		pos = lineEnd + 2;
+	}
+
+	if (pos < rawRequest.size()) {
+		request.body = rawRequest.substr(pos);
+	}
+
+	return request;
 }
--- ClientConnection.hpp
+++ ClientConnection.hpp
@@ -2,11 +2,32 @@
 #include <string>
 #include <unistd.h>
 
+struct HttpRequest
+{
+	std::string							method;
+	std::string							path;
+	std::string							version;
+	std::map<std::string, std::string>	headers;
+	std::string							body;
+};
+
+struct HttpResponse
+{
+	std::string							version;
+	int									statusCode;
+	std::string							statusMessage;
+	std::map<std::string, std::string>	headers;
+	std::string							body;
+};
+
 class ClientConnection {
 	private:
-		int _fd;
-		std::string _buffer;
-		bool _complete;
+		int				_fd;
+		std::string		_buffer;
+		bool			_complete;
+		HttpRequest		_request;
+		HttpResponse	_response;
+
 	public:
 		ClientConnection(int fd);
 		~ClientConnection();
@@ -15,4 +36,15 @@ class ClientConnection {
 		bool sendResponse(const std::string &response);
 		bool isRequestComplete() const { return _complete; }
 		int getFd() const { return _fd; }
+		HttpRequest parseRequest(const std::string &rawRequest);
+		// For debugging
+		void printRequest() const {
+			std::cout << "Method: " << _request.method << std::endl;
+			std::cout << "Path: " << _request.path << std::endl;
+			std::cout << "Version: " << _request.version << std::endl;
+			for (std::map<std::string, std::string>::const_iterator it = _request.headers.begin(); it != _request.headers.end(); ++it) {
+				std::cout << it->first << ": " << it->second << std::endl;
+			}
+			std::cout << "Body: " << _request.body << std::endl;
+		}
 };
